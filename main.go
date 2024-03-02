481f823352a679d7bcd96b5ec62473957bda9075
markkhomenko <silverstone909@tutamail.com>
2024-03-03 00:48:04 +0200

    add new routes

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tdewolff/canvas"
+)
+
+func main() {
+	svgFile, err := os.Create("example.svg")
+	if err != nil {
+		panic(err)
+	}
+	defer svgFile.Close()
+
+	svg := canvas.NewSVG(svgFile)
+	svg.AddFontFile("DejaVuSerif", canvas.Regular, "DejaVuSerif.ttf")
+	defer svg.Close()
+
+	font, err := svg.Font("DejaVuSerif")
+	if err != nil {
+		panic(err)
+	}
+
+	face := font.Face(12.0)
+	fmt.Println(face.Metrics())
+
+	plot := New("")
+	plot.Add(NewLine([]float64{0, 10, 20, 30, 40, 50, 60}, []float64{15, 25, 40, 30, 10, 5, 5}))
+	plot.Draw(svg, font, 80.0, 50.0)
+}
